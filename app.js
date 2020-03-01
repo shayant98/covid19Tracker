56caa9469f623b584611ef58dec5f811f34f7ede
Shayant Sital <shayant@gmail.com>
2020-02-29 21:08:44 -0300

    changedport

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const baseRoutes = require("./routes/baseRoutes")
 const app = express();
 app.set('view engine', 'ejs');
 app.set("views", "views")
-const port = 3000
+const port = process.env.PORT || 3000
 
 
 app.use("/", baseRoutes)
