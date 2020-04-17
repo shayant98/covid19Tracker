c3573de5a8532682643ba3946279d506a543a346
Shayant Sital <shayant@gmail.com>
2020-04-17 02:10:02 -0300

    map rework

--- public/js/Map.js
+++ public/js/Map.js
@@ -12,121 +12,94 @@ export default class map {
 
     init() {
 
-        this.mapbox.on('load', function () {
-            // Add a new source from our GeoJSON data and
-            // set the 'cluster' option to true. GL-JS will
-            // add the point_count property to your source data.
-            this.addSource('cases', {
-                type: 'geojson',
+        const nav = new mapboxgl.NavigationControl();
+        this.mapbox.addControl(nav, 'bottom-right');
 
-                data: `${window.location.href}api/geo/cases`,
-                cluster: true,
-                clusterMaxZoom: 10,
-                clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
-            });
-
-            this.addLayer({
-                id: 'clusters',
-                type: 'circle',
-                source: 'cases',
-                filter: ['has', 'point_count'],
-                paint: {
-
-                    'circle-color': "#ffc107",
-                    'circle-radius': [
-                        'step',
-                        ['get', 'point_count'],
-                        20,
-                        30,
-                        30,
-                        40,
-                        40
-                    ]
-                }
-            });
-
-
-            this.addLayer({
-                id: 'unclustered-point',
-                type: 'circle',
-                source: 'cases',
-                filter: ['!', ['has', 'point_count']],
-                paint: {
-                    'circle-color': '#ffc107',
-                    'circle-radius': 4,
-                    'circle-stroke-width': 1,
-                    'circle-stroke-color': '#000'
-                }
-            });
-
-            // inspect a cluster on click
-            this.on('click', 'clusters', (e) => {
-                const features = this.queryRenderedFeatures(e.point, {
-                    layers: ['clusters']
-                });
-                const clusterId = features[0].properties.cluster_id;
-                this.getSource('cases').getClusterExpansionZoom(
-                    clusterId,
-                    (err, zoom) => {
-                        if (err) return;
-
-                        this.easeTo({
-                            center: features[0].geometry.coordinates,
-                            zoom: zoom
-                        });
-                    }
-                );
-            });
-
-            // When a click event occurs on a feature in
-            // the unclustered-point layer, open a popup at
-            // the location of the feature, with
-            // description HTML from its properties.
-            this.on('click', 'unclustered-point', (e) => {
-
-                const object = e.features[0].properties
-                let confirmed
-                const coordinates = e.features[0].geometry.coordinates.slice();
-                const place = object.name;
-
-
-                const listItem = document.querySelector(`[data-id*='${place}']`)
-                if (listItem !== null) {
-                    const badgeContainer = listItem.childNodes[2].childNodes
-                    for (let index = 0; index < badgeContainer.length; index++) {
-                        const element = badgeContainer[index];
-                        if (element.classList.contains('badge-warning')) {
-                            confirmed = element.innerText
+        fetch(`${window.location.href}api/geo/cases`)
+            .then(response => response.json())
+            .then(data =>
+                this.mapbox.once('load', function () {
+                    this.addSource('points', {
+                        type: 'geojson',
+                        data: {
+                            type: 'FeatureCollection',
+                            features: data.features
                         }
-                    }
-                } else {
-                    confirmed = object.confirmed;
-                }
+                    });
+                    this.addLayer({
+                        id: 'circles',
+                        source: 'points',
+                        type: 'circle',
+                        paint: {
+                            'circle-opacity': 0.75,
+                            'circle-radius': [
+                                'interpolate', ['linear'],
+                                ['get', 'confirmed'],
+                                1,
+                                4,
+                                1000,
+                                8,
+                                4000,
+                                10,
+                                8000,
+                                14,
+                                12000,
+                                18,
+                                100000,
+                                40,
+                                250000,
+                                100
+                            ],
+                            'circle-color': '#EA240F'
+                        }
+                    });
+                    const popup = new mapboxgl.Popup({
+                        closeButton: false,
+                        closeOnClick: false
+                    });
 
 
+                    this.on('mousemove', 'circles', (e) => {
 
-                // Ensure that if the map is zoomed out such that
-                // multiple copies of the feature are visible, the
-                // popup appears over the copy being pointed to.
-                while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-                    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-                }
+                        const object = e.features[0].properties
+                        let confirmed
+                        const place = object.name;
 
-                new mapboxgl.Popup()
-                    .setLngLat(coordinates)
-                    .setHTML(`<span style='color:#000'>${place} has <strong>${confirmed}</strong> confirmed case(s)</span>`)
-                    .addTo(this);
-            });
 
-            this.on('mouseenter', 'clusters', () => {
-                this.getCanvas().style.cursor = 'pointer';
-            });
-            this.on('mouseleave', 'clusters', () => {
-                this.getCanvas().style.cursor = '';
-            });
-        });
+                        const listItem = document.querySelector(`[data-id*='${place}']`)
+                        if (listItem !== null) {
+                            const badgeContainer = listItem.childNodes[2].childNodes
+                            for (let index = 0; index < badgeContainer.length; index++) {
+                                const element = badgeContainer[index];
+                                if (element.classList.contains('badge-warning')) {
+                                    confirmed = element.innerText
+                                }
+                            }
+                        } else {
+                            confirmed = object.confirmed;
+                        }
+
+                        this.getCanvas().style.cursor = 'pointer';
+
+                        const coordinates = e.features[0].geometry.coordinates.slice();
+
+                        const HTML = `<span style='color:#000'>${place} has <strong>${confirmed}</strong> confirmed case(s)</span>`
+
+                        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+                        }
+                        popup.setLngLat(coordinates).setHTML(HTML).addTo(this);
+
+                    });
+
+                    this.on('mouseleave', 'circles', function () {
+                        this.getCanvas().style.cursor = '';
+                        popup.remove();
+                    });
+                })
+            );
+
     }
-
     showLocation(long, lat) {
         this.mapbox.flyTo({
             center: [
